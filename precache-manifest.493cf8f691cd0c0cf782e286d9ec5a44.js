43d1c7433fecb3b1fd70bc009f2c2445bd163b03
tenKre <tin_petrovic@yahoo.com>
2023-02-07 13:17:26 +0100

    Updates

---
+++ precache-manifest.493cf8f691cd0c0cf782e286d9ec5a44.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff07e68934ad6f6bcb30",
+    "url": "/pokemon-list-app/static/css/main.0258f98c.chunk.css"
+  },
+  {
+    "revision": "ff07e68934ad6f6bcb30",
+    "url": "/pokemon-list-app/static/js/main.ff07e689.chunk.js"
+  },
+  {
+    "revision": "dad8d1375f51e44f158b",
+    "url": "/pokemon-list-app/static/js/1.dad8d137.chunk.js"
+  },
+  {
+    "revision": "9d39a8f0a4a7e43326e9",
+    "url": "/pokemon-list-app/static/css/2.3d5380d8.chunk.css"
+  },
+  {
+    "revision": "9d39a8f0a4a7e43326e9",
+    "url": "/pokemon-list-app/static/js/2.9d39a8f0.chunk.js"
+  },
+  {
+    "revision": "94165b2de07301e89d87",
+    "url": "/pokemon-list-app/static/js/runtime~main.94165b2d.js"
+  },
+  {
+    "revision": "b6d07b13e961208a5f3b561c7e24ce6b",
+    "url": "/pokemon-list-app/static/media/pokeball-png-45331.b6d07b13.png"
+  },
+  {
+    "revision": "6563d6fbdf94ec958f5e089195e2a7d3",
+    "url": "/pokemon-list-app/static/media/pokeballs.6563d6fb.jpg"
+  },
+  {
+    "revision": "83a43bed200d9132395bbc9ba8d9a84e",
+    "url": "/pokemon-list-app/index.html"
+  }
+];
